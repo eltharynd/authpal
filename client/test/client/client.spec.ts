34fc0e99fedd35931743f6f4a109b5149d5b85d2
Laurent Schwitter <laurent.schwitter@gmail.com>
2022-08-25 18:47:17 +0200

    client unit test

--- client/test/client/client.spec.ts
+++ client/test/client/client.spec.ts
@@ -1,5 +1,136 @@
+import * as express from 'express'
+import * as request from 'supertest'
+import { AuthpalClient } from '../../src/client/client'
+import * as JWT from 'jsonwebtoken'
+
 describe('Client', () => {
-  it('should remind me to implement unit tests', () => {
-    expect(false).toBeTrue()
+  let app: express.Application
+  let authpalClient: AuthpalClient
+
+  beforeAll(() => {
+    app = global.app
+    authpalClient = global.authpalClient
+
+    global.userChangesEmitter.subscribe((changes) => {})
+    global.resumeDoneEmitter.subscribe((changes) => {})
+  })
+
+  it('should deny login', (done) => {
+    request(app)
+      .post('/login')
+      .send({ username: 'elthasarynd', password: 'asupersegacurepassword' })
+      .expect(401)
+      .end((err, res) => {
+        if (err) return done.fail(err)
+        done()
+      })
+  })
+
+  it('should login', (done) => {
+    request(app)
+      .post('/login')
+      .send({ username: 'eltharynd', password: 'asupersecurepassword' })
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done.fail(err)
+
+        let accessToken = res.body.accessToken
+        expect(accessToken).toBeDefined()
+        let cookie = res.headers['set-cookie'][0]
+        expect(/^refresh_token=.*;.*HttpOnly.*$/.test(cookie)).toBeTrue()
+
+        done()
+      })
+  })
+
+  it('should not authorize request', (done) => {
+    request(app)
+      .get('/me')
+      .expect(403)
+      .end((err, res) => {
+        if (err) return done.fail(err)
+        done()
+      })
+  })
+
+  it('should authorize request', (done) => {
+    request(app)
+      .post('/login')
+      .send({ username: 'eltharynd', password: 'asupersecurepassword' })
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done.fail(err)
+
+        let accessToken = res.body.accessToken
+        expect(accessToken).toBeDefined()
+
+        request(app)
+          .get('/me')
+          .set('Authorization', `Bearer ${accessToken}`)
+          .expect(200)
+          .end((err, res) => {
+            if (err) return done.fail(err)
+            done()
+          })
+      })
+  })
+
+  it('should not resume session', (done) => {
+    request(app)
+      .get('/resume')
+      .expect(401)
+      .end((err, res) => {
+        if (err) return done.fail(err)
+        done()
+      })
+  })
+
+  it('should resume session', (done) => {
+    request(app)
+      .post('/login')
+      .send({ username: 'eltharynd', password: 'asupersecurepassword' })
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done.fail(err)
+
+        let refreshToken = res.headers['set-cookie'][0]
+        expect(/^refresh_token=.*;.*HttpOnly.*$/.test(refreshToken)).toBeTrue()
+
+        request(app)
+          .get('/resume')
+          .set('Cookie', `${refreshToken}`)
+          .expect(200)
+          .end((err, res) => {
+            if (err) return done.fail(err)
+            let accessToken = res.body.accessToken
+            expect(accessToken).toBeDefined()
+            done()
+          })
+      })
+  })
+
+  it('should logout', (done) => {
+    request(app)
+      .post('/login')
+      .send({ username: 'eltharynd', password: 'asupersecurepassword' })
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done.fail(err)
+
+        let accessToken = res.body.accessToken
+        expect(accessToken).toBeDefined()
+        let refreshToken = res.headers['set-cookie'][0]
+        expect(/^refresh_token=.*;.*HttpOnly.*$/.test(refreshToken)).toBeTrue()
+
+        request(app)
+          .get('/logout')
+          .set('Authorization', `Bearer ${accessToken}`)
+          .set('Cookie', `${refreshToken}`)
+          .expect(200)
+          .end((err, res) => {
+            if (err) return done.fail(err)
+            done()
+          })
+      })
   })
 })
--- client/test/index.helper.spec.ts
+++ client/test/index.helper.spec.ts
@@ -1,3 +1,127 @@
+import * as express from 'express'
+import { filter, from, Subject, take } from 'rxjs'
+import { AuthpalClient } from '../src/client/client'
+import { Authpal } from '../../src/server/server'
+import { UserChangesEmitter } from '../src/client/interfaces'
+import * as bodyParser from 'body-parser'
+import * as cookieParser from 'cookie-parser'
+
 beforeAll((done) => {
+  global.users = [
+    {
+      id: 123,
+      username: 'eltharynd',
+      password: 'asupersecurepassword',
+    },
+  ]
+  global.sessions = []
+  global.authpal = new Authpal({
+    jwtSecret: 'asupersecretjwtsecret',
+    usernameField: 'username',
+    findUserByUsernameCallback: async (username) => {
+      let found: any = await from(global.users)
+        .pipe(
+          filter((u: any) => u.username === username),
+          take(1)
+        )
+        .toPromise()
+      if (found) {
+        return {
+          userid: found.id,
+        }
+      } else return null
+    },
+    findUserByIDCallback: async (userid) => {
+      let found: any = await from(global.users)
+        .pipe(
+          filter((u: any) => u.id === userid),
+          take(1)
+        )
+        .toPromise()
+      if (found)
+        return {
+          userid: found.id,
+        }
+      return null
+    },
+
+    findUserByRefreshToken: async (token) => {
+      let found: any = await from(global.users)
+        .pipe(
+          filter((u: any) => u.token.token === token),
+          take(1)
+        )
+        .toPromise()
+      if (found) {
+        return {
+          userid: found.id,
+        }
+      } else return null
+    },
+
+    verifyPasswordCallback: async (username, password) => {
+      let found: any = await from(global.users)
+        .pipe(
+          filter((u: any) => u.username === username),
+          take(1)
+        )
+        .toPromise()
+      return found && password === found.password
+    },
+    tokenRefreshedCallback: async (jwtPayload, token) => {
+      let found: any = await from(global.users)
+        .pipe(
+          filter((u: any) => u.id === jwtPayload.userid),
+          take(1)
+        )
+        .toPromise()
+      if (found) found.token = token
+    },
+    tokenDeletedCallback: async (jwtPayload, token) => {
+      let found: any = await from(global.users)
+        .pipe(
+          filter((u: any) => u.id === jwtPayload.userid),
+          take(1)
+        )
+        .toPromise()
+      if (found) found.token = null
+    },
+  })
+
+  global.app = express()
+  global.app.use(bodyParser.json())
+  global.app.use(cookieParser())
+
+  global.app.post('/login', global.authpal.loginMiddleWare)
+
+  global.app.get('/resume', global.authpal.resumeMiddleware)
+
+  global.app.get('/logout', global.authpal.logoutMiddleware)
+
+  global.app.get(
+    '/me',
+    global.authpal.authorizationMiddleware,
+    async (req, res) => {
+      let found: any = await from(global.users).pipe(
+        filter((u: any) => u.id === req.user.userid),
+        take(1)
+      )
+      res.json(found)
+    }
+  )
+
+  global.userChangesEmitter = new UserChangesEmitter()
+  global.resumeDoneEmitter = new Subject()
+
+  global.authPalClient = new AuthpalClient({
+    loginPostURL: `http://localhost:9999/auth/login`,
+    logoutGetURL: `http://localhost:9999/auth/logout`,
+    resumeGetURL: `http://localhost:9999/auth/resume`,
+
+    userChangesEmitter: global.userChangesEmitter,
+    resumeDoneEmitter: global.resumeDoneEmitter,
+    resumeDoneMiddleware: async (changes) => {},
+  })
+
   done()
 })
