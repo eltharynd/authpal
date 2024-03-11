1b39c610d38fee6236e87de3aff23898fa09a69f
Laurent Schwitter <laurent.schwitter@k-lab.ch>
2024-03-11 19:34:44 +0100

    optional googlePostUrl

--- client/src/client/interfaces.ts
+++ client/src/client/interfaces.ts
@@ -8,7 +8,7 @@ export interface AuthpalClientConfigs {
   resumeDoneEmitter: Subject<void>
   resumeDoneMiddleware?(changes?: UserChangesEvent): Promise<void>
 
-  googlePostUrl: string
+  googlePostUrl?: string
   loginPostURL: string
   resumeGetURL: string
   logoutGetURL: string
